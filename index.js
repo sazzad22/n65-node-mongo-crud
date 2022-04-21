db1bee8adffe3b61cb0a7542bf03b0fa70a0df50
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-21 15:03:58 +0600

    server init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World 2')
+})
+
+app.listen(port, () => console.log('server is running'));
